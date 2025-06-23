b1a0e2f5d79938476ed444a8b4d061eaaf560644
NiklasNeighbor <niklaswicker2005@gmail.com>
2025-06-23 13:09:27 +0200

    Camera now avoids the player falling out of frame from the bottom of the screen.

--- Assets/Scripts/Prototyping/Niklas/CameraControl.cs
+++ Assets/Scripts/Prototyping/Niklas/CameraControl.cs
@@ -5,6 +5,7 @@ public class CameraControl : MonoBehaviour
     public GameObject CameraTarget;
     public float VerticalMoveSpeed = 1f;
     public Vector2 IdleFraming;
+    public float LowerLimit = -4f;
     public LayerMask Ground;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,6 +28,11 @@ public class CameraControl : MonoBehaviour
         Vector3 currentPos = transform.position;
         Vector3 targetPos = new Vector3(transform.position.x, CameraTarget.transform.position.y + IdleFraming.y, transform.position.z);
 
+        while(CameraTarget.transform.position.y < transform.position.y + LowerLimit)
+        {
+            transform.Translate(Vector3.down * 0.1f);
+        }
+
         if (Vector3.Distance(currentPos, targetPos) > 0.5f)
         {
             if (Physics2D.BoxCast(CameraTarget.transform.position, new Vector2(1,1), 0f, Vector2.down, 0.1f, Ground))
@@ -42,5 +48,7 @@ public class CameraControl : MonoBehaviour
         Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y, 0) - new Vector3(IdleFraming.x, IdleFraming.y, 0), 0.5f);
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(CameraTarget.transform.position, 0.5f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(transform.position.x - 10, transform.position.y + LowerLimit, 0), new Vector3(transform.position.x + 10, transform.position.y + LowerLimit, 0));
     }
 }
