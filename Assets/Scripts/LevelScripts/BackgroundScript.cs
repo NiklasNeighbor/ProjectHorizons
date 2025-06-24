1f8bfa15cbaa0f581b41e44738fa40cf8ef599f8
KitusKatus <p4ul1901@gmail.com>
2025-06-25 01:17:06 +0200

    Working prototype

---
+++ Assets/Scripts/LevelScripts/BackgroundScript.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    [SerializeField] GameObject BackgroundParent;
+    [SerializeField] float SpawnDistance;
+    [SerializeField] float RemoveDistance;
+    [SerializeField] GameObject BackGroundPrefab;
+    [SerializeField] float Speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckLevelDistance();
+        ScrollAdvance(Speed);
+    }
+
+    void CheckLevelDistance()
+    {
+        Transform lastBg = BackgroundParent.transform.GetChild(BackgroundParent.transform.childCount - 1);
+        Vector3 spawnPos = lastBg.GetChild(0).GetChild(0).transform.position;
+        if (spawnPos.x < SpawnDistance)
+        {
+            SpawnLevel(spawnPos);
+        }
+
+        Transform firstChild = BackgroundParent.transform.GetChild(0);
+
+        if (firstChild.position.x < RemoveDistance)
+        {
+            Destroy(firstChild.gameObject);
+        }
+    }
+
+
+    void SpawnLevel(Vector3 pos)
+    {
+        GameObject newSegment = Instantiate(BackGroundPrefab, new Vector3(pos.x, pos.y, 0), Quaternion.identity);
+        newSegment.transform.parent = BackgroundParent.transform;
+    }
+
+    void ScrollAdvance(float moveAmount)
+    {
+        for (int i = 0; i < BackgroundParent.transform.childCount; i++)
+        {
+            BackgroundParent.transform.GetChild(i).position -= new Vector3(moveAmount, 0, 0);
+        }
+    }
+}
